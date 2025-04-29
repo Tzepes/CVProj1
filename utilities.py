f7b03ce30982973f1e1d4e46ef0997e1ae9973db
Tzepes <andreicristian.vlad@gmail.com>
2025-04-29 21:41:12 +0300

    updated utilities

--- utilities.py
+++ utilities.py
@@ -1,11 +1,26 @@
-import cv2 as cv
+import cv2 as cv2
+import matplotlib.pyplot as plt
 
-def show_image(image, window_name='image', timeout=0):
+def ShowImage(image, window_name='image', timeout=0):
     """
     :param timeout. How many seconds to wait untill it close the window.
     """
-    cv.namedWindow(window_name, cv.WINDOW_NORMAL)
-    cv.resizeWindow(window_name, 100, 100)  # Adjust the width and height as needed
-    cv.imshow(window_name, cv.resize(image, None, fx=0.6, fy=0.6))
-    cv.waitKey(timeout)
-    cv.destroyAllWindows()
+    img = image.copy()
+    cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
+    cv2.resizeWindow(window_name, 100, 100)  # Adjust the width and height as needed
+    cv2.imshow(window_name, cv2.resize(img, None, fx=0.6, fy=0.6))
+    cv2.waitKey(timeout)
+    cv2.destroyAllWindows()
+    
+def ShowKeypoints(image, keypoints, title="Keypoints"):
+    """
+    Show the keypoints on the image.
+    :param image: The image to show.
+    :param keypoints: The keypoints to show.
+    :param title: The title of the window.
+    """
+    img = cv2.drawKeypoints(image, keypoints, None, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+    plt.imshow(img)
+    plt.title(title)
+    plt.axis('off')
+    plt.show()
